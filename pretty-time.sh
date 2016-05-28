814f4399d1e0d82be8ce4f2b1ef30e109abfda04
Charlike Mike Reagent <tunnckoCore@users.noreply.github.com>
2016-05-29 00:17:14 +0300

    Create pretty-time.sh

---
+++ pretty-time.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if (( $# == 0 )); then
+	echo 'Input required'
+	exit 1
+fi
+
+pretty_time() {
+	local human total_seconds=$1
+	local days=$(( total_seconds / 60 / 60 / 24 ))
+	local hours=$(( total_seconds / 60 / 60 % 24 ))
+	local minutes=$(( total_seconds / 60 % 60 ))
+	local seconds=$(( total_seconds % 60 ))
+
+	(( days > 0 )) && human+="${days}d "
+	(( hours > 0 )) && human+="${hours}h "
+	(( minutes > 0 )) && human+="${minutes}m "
+	human+="${seconds}s"
+
+	echo "$human"	
+}
+
+pretty_time "$@"
